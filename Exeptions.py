5d1c64b79a4ef28e1f7de66462f5b477b862417d
andrew <o.andreyev@smartweb.com.ua>
2017-12-17 22:03:28 +0200

    adding Exeptions file

---
+++ Exeptions.py
@@ -0,0 +1,12 @@
+def exeption_handling():
+    car = {'make': 'bmw','model':007, 'year': 1999}
+
+    try:
+        print (car[color])
+    except:
+        print("Dictionary key error")
+    finally:
+        print ('Resolve finally anyway')
+
+
+exeption_handling()
