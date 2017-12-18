79270e8ea619d35dddd748e1e362abad40430970
andrew <o.andreyev@smartweb.com.ua>
2017-12-18 23:17:07 +0200

    fixing mispell in exeptions

--- Exeptions.py
+++ Exeptions.py
@@ -1,12 +1,12 @@
-def exeption_handling():
+def exception_handling():
     car = {'make': 'bmw','model': '007', 'year': 1999}
 
     try:
-        print (car[color])
+        print(car[color])
     except:
         print("Dictionary key error")
     finally:
         print ('Resolve finally anyway')
 
 
-exeption_handling()
+exception_handling()
