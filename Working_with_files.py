1efecf4f31b178a6d8e9f694e2d1aa835fb1f3a0
andrew <o.andreyev@smartweb.com.ua>
2017-12-23 17:11:02 +0200

    reading_files

--- Working_with_files.py
+++ Working_with_files.py
@@ -5,3 +5,7 @@ my_file = open("file.txt","w")
 for items in my_list:
     my_file.write(str(items) + '\n')
 my_file.close()
+
+my_file_new = open("file.txt","r")
+print (str(my_file_new.readline()))
+my_file_new.close()
