663c96102be1ad8ed9ab68af1fb9a95d2954b8a9
andrew <o.andreyev@smartweb.com.ua>
2017-12-23 19:30:21 +0200

    with as construction

--- Working_with_files.py
+++ Working_with_files.py
@@ -1,11 +1,17 @@
 my_list = [1,2,3,4,5]
 
 my_file = open("file.txt","w")
+print("Writing list of 5 items")
 
 for items in my_list:
     my_file.write(str(items) + '\n')
 my_file.close()
+print("Reading one_line")
 
 my_file_new = open("file.txt","r")
 print (str(my_file_new.readline()))
 my_file_new.close()
+
+print('with as construction reading all file')
+with open('file.txt','r') as f:
+    print(str(f.read()))
