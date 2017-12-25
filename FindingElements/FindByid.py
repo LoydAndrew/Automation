443791ca8bca186f7a461fe99ad78b8463e5b59e
andrew <o.andreyev@smartweb.com.ua>
2017-12-25 20:53:36 +0200

    FindingByid and name Prom

--- FindingElements/FindByid.py
+++ FindingElements/FindByid.py
@@ -2,18 +2,25 @@ from  selenium import webdriver
 
 class Findbyid(object):
     def test(self):
-        #baseUrl = "https://letskodeit.teachable.com/p/practice"
-        promUrl = 'https://prom.ua'
+        baseUrl = "https://letskodeit.teachable.com/p/practice"
         driver = webdriver.Firefox()
-        #driver.get(baseUrl)
-        driver.get(promUrl)
-        #elementById = driver.find_element_by_id("name")
-        #elementByName = driver.find_element_by_name("show-hide")
-        #if elementById is not None:
-            #print ("The name element was found by id")
-        elementByName = driver.find_element_by_name('search_term')
+        driver.get(baseUrl)
+        elementById = driver.find_element_by_id("name")
+        elementByName = driver.find_element_by_name("show-hide")
+        if elementById is not None:
+            print ("The 'name' element was found by id on Letskodeit")
         if elementByName is not None:
-            print ("The name element was found by name")
+            print ("The 'show-hide' element was found by name Letskodeit")
+
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        promUrl = 'https://prom.ua'
+        driver.get(promUrl)
+        elementByNameProm = driver.find_element_by_name('search_term')
+        elementByNameProm.send_keys("мобильные телефоны")
+        if elementByNameProm is not None:
+            print ("The 'search_term' element was found by name on Prom")
+
 
 FF = Findbyid()
 FF.test()
