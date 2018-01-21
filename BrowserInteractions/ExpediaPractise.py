92eb3686faeed2cc257b5cbee06fa2cec4ea61a4
andrew <o.andreyev@smartweb.com.ua>
2018-01-21 18:14:45 +0200

    Refactoring Expedia

--- BrowserInteractions/ExpediaPractise.py
+++ BrowserInteractions/ExpediaPractise.py
@@ -7,8 +7,8 @@ class ExpediaTest(object):
 
     def test(self):
         driver = webdriver.Chrome()
-        driver.get("https://www.expedia.com")
         driver.maximize_window()
+        driver.get("https://www.expedia.com")
         #driver.implicitly_wait(5)
 
         try:
@@ -17,6 +17,7 @@ class ExpediaTest(object):
             input_element = driver.find_element_by_id("hotel-destination-hp-hotel")
             input_element.send_keys("Kiev")
 
+            #choosing checkin and checkout dates
             driver.find_element_by_id('hotel-checkin-hp-hotel').send_keys(
                 '01/22/2018')
             sleep(1)
@@ -24,15 +25,19 @@ class ExpediaTest(object):
             checkout = driver.find_element_by_id('hotel-checkout-hp-hotel')
             checkout.clear()
             checkout.send_keys('01/28/2018')
-            sleep(2)
-
-            select_guests = Select(driver.find_element_by_css_selector(
-                "select[data-gcw-guests-toggled-field-on-value='99']"))
-            select_guests.select_by_value("1")
             sleep(1)
-
-            driver.find_element_by_id('hotel-add-car-checkbox-hp-hotel').click()
+            #clicking on dropdown and choosing number of guests
+            guests_element = driver.find_element_by_css_selector(
+                "select[data-gcw-guests-toggled-field-on-value='99']")
+            guests_element.click()
+            select_guests = Select(guests_element).select_by_value('1')
+            sleep(1)
+            #choosing car option
+            car_selected = driver.find_element_by_id('hotel-add-car-checkbox-hp-hotel')
+            car_selected.click()
+            print("Car option is selected: " + str(car_selected.is_enabled()))
             #sleep(1)
+            #submiting all that was selected
             driver.find_element_by_css_selector(
                 '#gcw-hotel-form-hp-hotel [type="submit"]').click()
 
