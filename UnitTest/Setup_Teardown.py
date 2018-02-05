c73f19ed2389f6acd0fb02b5d1b5aaf32dbd68f8
andrew <o.andreyev@smartweb.com.ua>
2018-02-05 05:21:34 +0200

    setup and teardown

---
+++ UnitTest/Setup_Teardown.py
@@ -0,0 +1,24 @@
+import unittest
+
+class TestCase1(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print ("#" * 30)
+        print("Running only once before every test")
+        print("#" * 30)
+
+    def test_method1(self):
+        print("\nRunning 1 method")
+
+    def test_method2(self):
+        print("Running 2 method")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("#" * 30)
+        print ("Running only once after every test")
+        print("#" * 30)
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
